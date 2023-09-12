a681233423e9f1dace111cb8e95b73a653719745
Martelka23 <isupov.2001@mail.ru>
2023-09-13 00:42:18 +0300

    Timestamp fix

--- src/generator/generator.ts
+++ src/generator/generator.ts
@@ -111,8 +111,8 @@ class SqlGenerator {
         let rangeString = start === 1 ? 'WHERE ' : '';
         let lastIndex = start;
         let rangeValues: Array<number | string> = [];
-        const isMs1 = args.toTimestamp && args.from.toString().length === 13;
-        const isMs2 = args.toTimestamp && args.to.toString().length === 13;
+        const isMs1 = args.toTimestamp && args.from?.toString()?.length === 13;
+        const isMs2 = args.toTimestamp && args.to?.toString()?.length === 13;
         const value1 = args.toTimestamp ? `to_timestamp($${lastIndex}${isMs1 ? '::double precision / 1000' : ''})` : `$${lastIndex}`;
         const value2 = args.toTimestamp ? `to_timestamp($${lastIndex + 1}${isMs2 ? '::double precision / 1000' : ''})` : `$${lastIndex + 1}`;
 
